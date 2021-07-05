59b1415beb4978d60d10555a04012287e0f46272
Francesco <fdenaro96@gmail.com>
2021-07-05 17:47:04 +0200

    Creating NewExpense component and its child ExpenseForm structure to add new item to the list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from "react";
 import Expenses from './components/Expenses/Expenses';
-
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
 
@@ -12,8 +13,8 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses expensesList={expenses}/>
-      
     </div>
   )
 }
